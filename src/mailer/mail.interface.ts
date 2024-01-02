f0aae7a57b254f37666e6edb38814469d101b4cc
miguel <jmtiradop19@gmail.com>
2024-01-02 18:08:48 -0500

    feature: sendemail

---
+++ src/mailer/mail.interface.ts
@@ -0,0 +1,11 @@
+/* eslint-disable prettier/prettier */
+import { Address } from "nodemailer/lib/mailer";
+
+export type SendEmailDto = {
+  from?: Address,
+  recipients: Address[],
+  subject: string;
+  html: string;
+  text?: string;
+  placeholderReplacements?: Record<string, string>;
+}
